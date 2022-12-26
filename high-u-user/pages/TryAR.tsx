64dc4b6153b602be8b3ff95252ea4a088cefa60d
Heart Wirunrom <heart569522@gmail.com>
2022-12-27 01:23:20 +0700

    Update TryAR.tsx

--- high-u-user/pages/TryAR.tsx
+++ high-u-user/pages/TryAR.tsx
@@ -4,7 +4,7 @@ import {
     Paper,
     Container,
     Typography,
-    Icon,
+    IconButton,
     Modal,
     Button,
     Card,
@@ -12,7 +12,7 @@ import {
     CardContent,
     CardActionArea,
 } from '@mui/material'
-import CameraIcon from '@mui/icons-material/Camera';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import Webcam from 'react-webcam';
 import { createTheme, ThemeProvider, } from '@mui/material/styles';
 
@@ -36,6 +36,12 @@ const theme = createTheme({
 
 });
 
+const videoConstraints = {
+    width: 720,
+    height: 1280,
+    facingMode: "user"
+};
+
 export default function TryAR() {
 
     const [modalOpen, setModalOpen] = useState(false);
@@ -51,32 +57,36 @@ export default function TryAR() {
     return (
         <div>
             <ThemeProvider theme={theme}>
-                <Paper className="bg-[#252525]">
+                <Paper className="bg-[#252525] h-screen" >
                     <Navbar />
                     <Container maxWidth="xl" >
                         <Grid container>
-                            <Grid xs={12} className="py-2 ">
+                            <Grid xs={12} className="pt-4 ">
                                 <Typography variant='h3' className='text-[#F0CA83] text-center max-sm:text-[36px] font-bold'>TRY AR</Typography>
                             </Grid>
-                            <Grid xs={12} className="pb-4 ">
+                            <Grid xs={12} className="pb-2 ">
                                 <Typography variant='h5' className="text-[#F0CA83] text-center max-sm:text-base">Snap photos and share like never before</Typography>
                             </Grid>
                         </Grid>
                         <Grid container spacing={2} className="py-5">
-                            <Grid xs={12} sm={6}>
+                            <Grid item sm={12} md={6} spacing={2}>
                                 <div className="w-full h-full ">
                                     <Webcam
-                                        className="border-2 border-gray-400 flip"
-                                        width='720'
+                                        className="border-2 border-[#F0CA83]"
+                                        width={720}
+                                        height={1280}
                                         ref={webcamRef}
                                         screenshotFormat="image/jpeg"
-                                        style={{ transform: 'scaleX(-1)' }}
+                                        mirrored
+                                        videoConstraints={videoConstraints}
                                     />
-                                    <Button onClick={capture} className="w-4 h-4">
-                                        <Icon>
-                                            <CameraIcon />
-                                        </Icon>
-                                    </Button>
+                                    <Grid item xs={12} className="pt-3">
+                                        <Button onClick={capture} variant="contained" className="block m-auto bg-[#F0CA83]">
+                                            <IconButton className="text-gray-800">
+                                                <CameraAltIcon fontSize='large' />
+                                            </IconButton>
+                                        </Button>
+                                    </Grid>
                                     <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
                                         <div className="w-full h-full p-4 rounded-lg shadow-lg">
                                             <img src={image as string | undefined} alt="Captured image" />
