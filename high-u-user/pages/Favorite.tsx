f4a6f76f9ef2c5f47d5b3e1aa24dabaf0bd99a69
Heart Wirunrom <heart569522@gmail.com>
2023-01-02 02:51:43 +0700

    tab menu

--- high-u-user/pages/Favorite.tsx
+++ high-u-user/pages/Favorite.tsx
@@ -9,22 +9,73 @@ import {
     TextField,
     CssBaseline,
     Button,
-    Paper
+    Paper,
+    Tab,
+    Tabs
 } from '@mui/material';
+import {
+    TabContext,
+    TabList,
+    TabPanel,
+} from '@mui/lab';
+import { createTheme, ThemeProvider, } from '@mui/material/styles';
 
 // IMPORT COMPONENT
 import Navbar from "../components/Navigation/Navigation"
 import UserHeader from '../components/Auth/UserHeader';
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#F0CA83",
+        },
+        secondary: {
+            main: "#F08383",
+        },
+    },
+    typography: {
+        fontFamily: [
+            'Prompt, sans-serif'
+        ].join(','),
+    },
+
+});
+
 export default function Favorite() {
+    const [value, setValue] = React.useState('1');
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
+
+
     return (
         <div>
-            <Paper className="bg-[#252525] h-screen">
-                <Navbar />
-                <Container maxWidth="xl" >
-                    <UserHeader />
-                </Container>
-            </Paper>
+            <ThemeProvider theme={theme}>
+                <Paper className="bg-[#252525] h-screen">
+                    <Navbar />
+                    <Container maxWidth="xl" >
+                        <UserHeader />
+                        <Box className="w-full py-6" sx={{ typography: 'body1' }}>
+                            <TabContext value={value}>
+                                <Box className="border-b border-[#886828]">
+                                    <TabList onChange={handleChange} className="text-[#F0CA83]" aria-label="Favorite Menu">
+                                        <Tab label="My Favorite Wigs" value="1" />
+                                        <Tab label="My AR Images" value="2" />
+                                    </TabList>
+                                </Box>
+                                <TabPanel value="1">
+
+                                </TabPanel>
+                                <TabPanel value="2">
+                                    
+                                </TabPanel>
+                            </TabContext>
+                        </Box>
+                    </Container>
+                </Paper>
+            </ThemeProvider>
+
 
         </div>
     );
