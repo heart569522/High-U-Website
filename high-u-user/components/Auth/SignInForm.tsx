0bc3dc8c86286eabf306f4369934a6b87ff02f3d
Heart Wirunrom <heart569522@gmail.com>
2023-01-05 05:17:29 +0700

    fix bg gold

--- high-u-user/components/Auth/SignInForm.tsx
+++ high-u-user/components/Auth/SignInForm.tsx
@@ -65,70 +65,72 @@ const SignInForm = React.memo(() => {
     return (
 
         <ThemeProvider theme={theme}>
-            <Box className="colorBackgroundGold h-full w-full bg-cover fixed" >
-                <Container component="main" maxWidth="sm">
-                    <CssBaseline />
-                    <Box className="dropShadow mt-8 flex flex-col items-center bg-white p-5 rounded-sm" >
-                        <Typography component="h1" variant="h5" color="primary" className="font-bold">
-                            High U - Member
-                        </Typography>
-                        <Box component="form" onSubmit={handleLogin} sx={{ mt: 1 }}>
-                            <TextField
-                                label="Username"
-                                value={username}
-                                id="username"
-                                name="username"
-                                onChange={handleUsernameChange}
-                                onKeyPress={handleKeyPress}
-                                margin="normal"
-                                required
-                                fullWidth
-                                autoComplete="username"
-                                autoFocus
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                name="password"
-                                label="Password"
-                                value={password}
-                                id="password"
-                                autoComplete="current-password"
-                                onChange={handlePasswordChange}
-                                onKeyPress={handleKeyPress}
-                                type={showPassword ? 'text' : 'password'}
-                                variant="outlined"
-                                fullWidth
-                                InputProps={{
-                                    endAdornment: (
-                                        <IconButton
-                                            aria-label="toggle password visibility"
-                                            onClick={handleClickShowPassword}
-                                            onMouseDown={handleMouseDownPassword}
-                                        >
-                                            {showPassword ? <Visibility /> : <VisibilityOff />}
-                                        </IconButton>
-                                    ),
-                                }}
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                className="bg-amber-400 mt-3 mb-2 font-bold"
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container>
-                                <Grid item>
-                                    <Link href="/SignUp" variant="body2" color="secondary">
-                                        {"Don't have an account? SignUp"}
-                                    </Link>
+            <Box className="bg-[#948975] h-full w-full bg-cover fixed">
+                <Box className="colorBackgroundGold h-full w-full bg-cover fixed" >
+                    <Container component="main" maxWidth="sm">
+                        <CssBaseline />
+                        <Box className="dropShadow mt-8 flex flex-col items-center bg-white p-5 rounded-sm" >
+                            <Typography component="h1" variant="h5" color="primary" className="font-bold">
+                                High U - Member
+                            </Typography>
+                            <Box component="form" onSubmit={handleLogin} sx={{ mt: 1 }}>
+                                <TextField
+                                    label="Username"
+                                    value={username}
+                                    id="username"
+                                    name="username"
+                                    onChange={handleUsernameChange}
+                                    onKeyPress={handleKeyPress}
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    autoComplete="username"
+                                    autoFocus
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    name="password"
+                                    label="Password"
+                                    value={password}
+                                    id="password"
+                                    autoComplete="current-password"
+                                    onChange={handlePasswordChange}
+                                    onKeyPress={handleKeyPress}
+                                    type={showPassword ? 'text' : 'password'}
+                                    variant="outlined"
+                                    fullWidth
+                                    InputProps={{
+                                        endAdornment: (
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                            >
+                                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                                            </IconButton>
+                                        ),
+                                    }}
+                                />
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    className="bg-amber-400 mt-3 mb-2 font-bold"
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    <Grid item>
+                                        <Link href="/SignUp" variant="body2" color="secondary">
+                                            {"Don't have an account? SignUp"}
+                                        </Link>
+                                    </Grid>
                                 </Grid>
-                            </Grid>
+                            </Box>
                         </Box>
-                    </Box>
-                </Container>
+                    </Container>
+                </Box>
             </Box>
         </ThemeProvider>
 
--- high-u-user/components/Auth/SignUpForm.tsx
+++ high-u-user/components/Auth/SignUpForm.tsx
@@ -41,48 +41,48 @@ export default function SignUpForm() {
     const [confirmPassword, setConfirmPassword] = useState<string>("");
     // const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    
+
     const handleChange = (setState: (value: string) => void) => (
         event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-      ) => {
+    ) => {
         setState(event.target.value);
-      };
-    
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
-      const { value } = event.target;
-      setPassword(value);
+    };
 
-      if (value !== confirmPassword) {
-        setError("The passwords do not match.");
-      } else {
-        setError(null);
-      }
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setPassword(value);
+
+        if (value !== confirmPassword) {
+            setError("The passwords do not match.");
+        } else {
+            setError(null);
+        }
 
     };
-    
+
     const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { value } = event.target;
-      setConfirmPassword(value);
-    
-      if (value !== password) {
-        setError("The passwords do not match.");
-      } else {
-        setError(null);
-      }
+        const { value } = event.target;
+        setConfirmPassword(value);
+
+        if (value !== password) {
+            setError("The passwords do not match.");
+        } else {
+            setError(null);
+        }
     };
-    
+
     // const handleClickShowConfirmPassword = () => {
     //   setShowConfirmPassword(!showConfirmPassword);
     // };
-    
+
     // const handleMouseDownConfirmPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     //   event.preventDefault();
     // };
-    
+
     const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-      if (event.key === " ") {
-        event.preventDefault();
-      }
+        if (event.key === " ") {
+            event.preventDefault();
+        }
     };
 
     const handleReset = () => {
@@ -94,106 +94,107 @@ export default function SignUpForm() {
         setConfirmPassword("");
         setError(null);
     };
-    
+
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      alert(username)
-      alert(password)
+        event.preventDefault();
+        alert(username)
+        alert(password)
     };
 
     return (
 
         <ThemeProvider theme={theme}>
-            <Box className="colorBackgroundGold h-full w-full bg-cover fixed" >
-                <Container component="main" maxWidth="sm">
-                    <CssBaseline />
-                    <Box className="dropShadow mt-8 flex flex-col items-center bg-white p-5 rounded-sm" >
-                        <Typography component="h1" variant="h5" color="primary" className="font-bold">
-                            High U - Sign Up
-                        </Typography>
-                        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                            <Grid container spacing={1}>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        autoComplete="given-name"
-                                        name="firstName"
-                                        value={firstname}
-                                        required
-                                        fullWidth
-                                        id="firstName"
-                                        label="First Name"
-                                        autoFocus
-                                        onChange={handleChange(setFirstname)}
-                                        onKeyPress={handleKeyPress}
-                                    />
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        value={lastname}
-                                        id="lastName"
-                                        label="Last Name"
-                                        name="lastName"
-                                        autoComplete="family-name"
-                                        onChange={handleChange(setLastname)}
-                                        onKeyPress={handleKeyPress}
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        label="Username"
-                                        value={username}
-                                        id="username"
-                                        name="username"
-                                        onChange={handleChange(setUsername)}
-                                        margin="normal"
-                                        required
-                                        fullWidth
-                                        autoComplete="username"
-                                        autoFocus
-                                        onKeyPress={handleKeyPress}
-                                    />
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        value={email}
-                                        id="email"
-                                        label="Email Address"
-                                        name="email"
-                                        autoComplete="email"
-                                        onChange={handleChange(setEmail)}
-                                        onKeyPress={handleKeyPress}
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        margin="normal"
-                                        required
-                                        name="password"
-                                        label="Password"
-                                        value={password}
-                                        id="password"
-                                        autoComplete="current-password"
-                                        onChange={handlePasswordChange}
-                                        variant="outlined"
-                                        fullWidth
-                                        onKeyPress={handleKeyPress}
-                                    />
-                                    <TextField
-                                        required
-                                        name="confirmPassword"
-                                        label="Confirm Password"
-                                        value={confirmPassword}
-                                        id="confirmPassword"
-                                        onChange={handleConfirmPasswordChange}
-                                        // type={showConfirmPassword ? 'text' : 'password'}
-                                        type={password}
-                                        variant="outlined"
-                                        onKeyPress={handleKeyPress}
-                                        fullWidth
-                                        error={Boolean(error)}
-                                        helperText={error}
+            <Box className="bg-[#948975] h-full w-full bg-cover fixed">
+                <Box className="colorBackgroundGold h-full w-full bg-cover fixed" >
+                    <Container component="main" maxWidth="sm">
+                        <CssBaseline />
+                        <Box className="dropShadow mt-8 flex flex-col items-center bg-white p-5 rounded-sm" >
+                            <Typography component="h1" variant="h5" color="primary" className="font-bold">
+                                High U - Sign Up
+                            </Typography>
+                            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                                <Grid container spacing={1}>
+                                    <Grid item xs={12} sm={6}>
+                                        <TextField
+                                            autoComplete="given-name"
+                                            name="firstName"
+                                            value={firstname}
+                                            required
+                                            fullWidth
+                                            id="firstName"
+                                            label="First Name"
+                                            autoFocus
+                                            onChange={handleChange(setFirstname)}
+                                            onKeyPress={handleKeyPress}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            value={lastname}
+                                            id="lastName"
+                                            label="Last Name"
+                                            name="lastName"
+                                            autoComplete="family-name"
+                                            onChange={handleChange(setLastname)}
+                                            onKeyPress={handleKeyPress}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            label="Username"
+                                            value={username}
+                                            id="username"
+                                            name="username"
+                                            onChange={handleChange(setUsername)}
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            autoComplete="username"
+                                            autoFocus
+                                            onKeyPress={handleKeyPress}
+                                        />
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            value={email}
+                                            id="email"
+                                            label="Email Address"
+                                            name="email"
+                                            autoComplete="email"
+                                            onChange={handleChange(setEmail)}
+                                            onKeyPress={handleKeyPress}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            margin="normal"
+                                            required
+                                            name="password"
+                                            label="Password"
+                                            value={password}
+                                            id="password"
+                                            autoComplete="current-password"
+                                            onChange={handlePasswordChange}
+                                            variant="outlined"
+                                            fullWidth
+                                            onKeyPress={handleKeyPress}
+                                        />
+                                        <TextField
+                                            required
+                                            name="confirmPassword"
+                                            label="Confirm Password"
+                                            value={confirmPassword}
+                                            id="confirmPassword"
+                                            onChange={handleConfirmPasswordChange}
+                                            // type={showConfirmPassword ? 'text' : 'password'}
+                                            type={password}
+                                            variant="outlined"
+                                            onKeyPress={handleKeyPress}
+                                            fullWidth
+                                            error={Boolean(error)}
+                                            helperText={error}
                                         // InputProps={{
                                         //     endAdornment: (
                                         //         <IconButton
@@ -205,38 +206,39 @@ export default function SignUpForm() {
                                         //         </IconButton>
                                         //     ),
                                         // }}
-                                    />
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Button
+                                            type="submit"
+                                            fullWidth
+                                            variant="contained"
+                                            className="bg-amber-400 mt-3 mb-2 font-bold"
+                                        >
+                                            Sign Up
+                                        </Button>
+                                        <Button
+                                            type="reset"
+                                            fullWidth
+                                            variant="outlined"
+                                            onClick={handleReset}
+                                            className="text-red-500 border-red-500 hover:border-red-700 hover:text-red-700 mt-3 mb-2 font-bold"
+                                        >
+                                            Reset
+                                        </Button>
+                                    </Grid>
                                 </Grid>
-                                <Grid item xs={12}>
-                                    <Button
-                                        type="submit"
-                                        fullWidth
-                                        variant="contained"
-                                        className="bg-amber-400 mt-3 mb-2 font-bold"
-                                    >
-                                        Sign Up
-                                    </Button>
-                                    <Button
-                                        type="reset"
-                                        fullWidth
-                                        variant="outlined"
-                                        onClick={handleReset}
-                                        className="text-red-500 border-red-500 hover:border-red-700 hover:text-red-700 mt-3 mb-2 font-bold"
-                                    >
-                                        Reset
-                                    </Button>
+                                <Grid container justifyContent="flex-end">
+                                    <Grid item>
+                                        <Link href="/SignIn" variant="body2" color="secondary">
+                                            {"Already have an account? Sign in"}
+                                        </Link>
+                                    </Grid>
                                 </Grid>
-                            </Grid>
-                            <Grid container justifyContent="flex-end">
-                                <Grid item>
-                                    <Link href="/SignIn" variant="body2" color="secondary">
-                                        {"Already have an account? Sign in"}
-                                    </Link>
-                                </Grid>
-                            </Grid>
+                            </Box>
                         </Box>
-                    </Box>
-                </Container>
+                    </Container>
+                </Box>
             </Box>
         </ThemeProvider>
 
