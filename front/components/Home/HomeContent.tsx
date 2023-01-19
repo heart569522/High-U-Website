b90c58f4f937d20cfaec374c5a4a1e44a7f0ed91
Heart Wirunrom <heart569522@gmail.com>
2023-01-19 18:40:10 +0700

    test

--- front/components/Home/HomeContent.tsx
+++ front/components/Home/HomeContent.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import { useRouter } from 'next/router'
 import Link from 'next/link';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import {
   Box,
   Button,
@@ -38,18 +40,28 @@ export default function HomeContent() {
     router.push(path)
   }
 
+  useEffect(() => {
+    Aos.init({
+      duration : 2000
+    })
+  }, []);
+
+  useEffect(() => {
+    Aos.refresh();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Box className="bg-[#303030] h-max py-10">
         <Container maxWidth="xl">
           <Grid container>
-            <Grid item xs={12} sm={6} className="flex items-center justify-end max-sm:justify-center">
+            <Grid item xs={12} sm={6} className="flex items-center justify-end max-sm:justify-center" data-aos="fade-up">
               <img
                 className="w-full max-w-lg p-3"
                 src="https://t4.ftcdn.net/jpg/04/27/41/05/360_F_427410504_H6zGuHXoNdaH7Ghwpou1dXgbODusZeAg.jpg"
               />
             </Grid>
-            <Grid item xs={12} sm={6} className="flex items-center text-left max-sm:text-center">
+            <Grid item xs={12} sm={6} className="flex items-center text-left max-sm:text-center" data-aos="fade-up">
               <Typography
                 className="text-2xl px-3 max-sm:text-lg"
               >
@@ -62,14 +74,14 @@ export default function HomeContent() {
       <Box className="bg-[#787878] h-max py-10">
         <Container maxWidth="xl">
           <Grid container className="max-sm:flex-wrap-reverse">
-            <Grid item xs={12} sm={6} className="flex items-center text-right max-sm:text-center">
+            <Grid item xs={12} sm={6} className="flex items-center text-right max-sm:text-center" data-aos="fade-up">
               <Typography
                 className="text-2xl p-3 max-sm:text-lg"
               >
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
               </Typography>
             </Grid>
-            <Grid item xs={12} sm={6} className="flex items-center justify-start max-sm:justify-center ">
+            <Grid item xs={12} sm={6} className="flex items-center justify-start max-sm:justify-center " data-aos="fade-up">
               <img
                 className="w-full max-w-lg px-3"
                 src="https://t4.ftcdn.net/jpg/04/27/41/49/360_F_427414979_2GhGAcGPFFUwfANjcpurjis6lVrU45Bx.jpg"
