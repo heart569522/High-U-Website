24e045d478684220963b6397cbf42102d1f3a08a
Heart Wirunrom <heart569522@gmail.com>
2023-01-26 18:11:43 +0700

    webview chart

--- front/components/Chart/WebsiteView_AreaChart.tsx
+++ front/components/Chart/WebsiteView_AreaChart.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { AreaChart, } from "@tremor/react";
+import _ from 'lodash'
 
 import Website_View from '../../helper/Website_View.json'
 
 export default function WebsiteView_AreaChart() {
-    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    Website_View.map(data => {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const groupByDay = _.groupBy(Website_View, data => {
         let date = new Date(data.date);
-        data.day = days[date.getUTCDay()]
+        return days[date.getUTCDay()]
+    });
+    let chartDayData: any[] = []
+    Object.keys(groupByDay).forEach(day => {
+        let totalViews = groupByDay[day].reduce((acc, curr) => acc + curr.view, 0)
+        chartDayData.push({ day, view: totalViews })
     })
+    chartDayData.sort((a, b) => days.indexOf(a.day) - days.indexOf(b.day))
 
     return (
         <div>
             <AreaChart
-                data={Website_View}
+                data={chartDayData}
                 dataKey="day"
                 categories={["view"]}
                 colors={["red",]}
