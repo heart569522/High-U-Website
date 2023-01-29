7ba6660f0d9c8be6bdbda66828fb9f56e58ffb63
Heart Wirunrom <heart569522@gmail.com>
2023-01-29 18:54:21 +0700

    add skeleton in table admin

--- front/components/Table/MemberList_Table.tsx
+++ front/components/Table/MemberList_Table.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
 import {
     Table,
@@ -21,6 +21,7 @@ import {
     Button,
     Modal,
     Divider,
+    Skeleton,
 } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
@@ -45,6 +46,15 @@ const theme = createTheme({
 
 
 function MemberList_Table() {
+    const [loading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // Fetch data
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
+    }, [loading]);
+
     const [selectedId, setSelectedId] = useState(0);
 
     const [page, setPage] = useState(0);
@@ -70,89 +80,101 @@ function MemberList_Table() {
                 <Box className="bg-white w-full h-full rounded-xl pt-5 px-5 shadow-md max-[899px]:pb-3">
                     <Grid container>
                         <Grid item xs={12} md={12} className="flex items-center justify-between max-md:mb-3">
-                            <Typography className="text-[#303030] font-bold text-2xl">
-                                Member Manage
-                            </Typography>
-                            <Link href="/admin/AddMember">
-                                <Button className="text-white font-bold px-5 text-center shadow bg-[#303030] hover:bg-[#555555]">Add Member</Button>
-                            </Link>
+                            {loading ? (<Skeleton animation="wave" variant="text" className="w-1/5 text-5xl rounded-md" />) : (
+                                <Typography className="text-[#303030] font-bold text-2xl">
+                                    Member Manage
+                                </Typography>
+                            )}
+                            {loading ? (<Skeleton animation="wave" variant="text" className="w-1/5 text-5xl rounded-md" />) : (
+                                <Link href="/admin/AddMember">
+                                    <Button className="text-white font-bold px-5 text-center shadow bg-[#303030] hover:bg-[#555555]">Add Member</Button>
+                                </Link>
+                            )}
                         </Grid>
                         <Hidden mdDown >
                             <TableContainer className="mt-3 rounded-md">
-                                <Table className="">
-                                    <TableHead>
-                                        <TableRow className=" bg-[#F0CA83]">
-                                            <TableCell className="w-[25%] text-lg text-center font-bold">Image</TableCell>
-                                            <TableCell className="w-[12%] text-lg font-bold">Firstname</TableCell>
-                                            <TableCell className="w-[12%] text-lg font-bold">Lastname</TableCell>
-                                            <TableCell className="w-[12%] text-lg font-bold">Email</TableCell>
-                                            <TableCell className="w-[12%] text-lg font-bold">Username</TableCell>
-                                            <TableCell className="w-[12%] text-lg font-bold">Password</TableCell>
-                                            <TableCell className="w-[15%] text-lg text-center font-bold">Settings</TableCell>
-                                        </TableRow>
-                                    </TableHead>
+                                <Table>
+                                    {loading ? (<Skeleton animation="wave" variant="rectangular" className="w-full h-12 rounded-md" />) : (
+                                        <TableHead>
+                                            <TableRow className=" bg-[#F0CA83]">
+                                                <TableCell className="w-[25%] text-lg text-center font-bold">Image</TableCell>
+                                                <TableCell className="w-[12%] text-lg font-bold">Firstname</TableCell>
+                                                <TableCell className="w-[12%] text-lg font-bold">Lastname</TableCell>
+                                                <TableCell className="w-[12%] text-lg font-bold">Email</TableCell>
+                                                <TableCell className="w-[12%] text-lg font-bold">Username</TableCell>
+                                                <TableCell className="w-[12%] text-lg font-bold">Password</TableCell>
+                                                <TableCell className="w-[15%] text-lg text-center font-bold">Settings</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                    )}
                                     <TableBody>
                                         {Member_Data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, i) => (
-                                            <TableRow key={item.id} className="hover:bg-gray-50">
-                                                <TableCell className="flex justify-center items-center">
-                                                    <img src={item.image} className="object-top rounded-lg object-cover h-40 w-40 max-xl:h-36 max-xl:w-36 max-[1075px]:h-32 max-[1000px]:h-24" />
-                                                </TableCell>
-                                                <TableCell className="w-[12%] text-base">{item.firstname}</TableCell>
-                                                <TableCell className="w-[12%] text-base">{item.lastname}</TableCell>
-                                                <TableCell className="w-[12%] text-base">{item.email}</TableCell>
-                                                <TableCell className="w-[12%] text-base">{item.username}</TableCell>
-                                                <TableCell className="w-[12%] text-base">{item.password}</TableCell>
-                                                <TableCell className="w-[15%] text-center ">
-                                                    <ButtonGroup variant="contained" className="gap-1" aria-label="contained button group">
-                                                        <Link href="/admin/MemberEdit/[id]" as={`/admin/MemberEdit/${item.id}`}>
-                                                            <Button className="bg-[#303030] text-white hover:bg-amber-500">Edit</Button>
-                                                        </Link>
-                                                        {/* <Link href="/WigEdit/[id]" as={`/WigEdit/${item.id}`}> */}
-                                                        <Button className="bg-[#303030] text-white hover:bg-red-500">Delete</Button>
-                                                        {/* </Link> */}
-                                                    </ButtonGroup>
+                                            loading ? (<Skeleton key={item.id} animation="wave" variant="rectangular" className="w-full h-28 my-4 rounded-md" />) : (
+                                                <TableRow key={item.id} className="hover:bg-gray-50">
+                                                    <TableCell className="flex justify-center items-center">
+                                                        <img src={item.image} className="object-top rounded-lg object-cover h-40 w-40 max-xl:h-36 max-xl:w-36 max-[1075px]:h-32 max-[1000px]:h-24" />
+                                                    </TableCell>
+                                                    <TableCell className="w-[12%] text-base">{item.firstname}</TableCell>
+                                                    <TableCell className="w-[12%] text-base">{item.lastname}</TableCell>
+                                                    <TableCell className="w-[12%] text-base">{item.email}</TableCell>
+                                                    <TableCell className="w-[12%] text-base">{item.username}</TableCell>
+                                                    <TableCell className="w-[12%] text-base">{item.password}</TableCell>
+                                                    <TableCell className="w-[15%] text-center ">
+                                                        <ButtonGroup variant="contained" className="gap-1" aria-label="contained button group">
+                                                            <Link href="/admin/MemberEdit/[id]" as={`/admin/MemberEdit/${item.id}`}>
+                                                                <Button className="bg-[#303030] text-white hover:bg-amber-500">Edit</Button>
+                                                            </Link>
+                                                            {/* <Link href="/WigEdit/[id]" as={`/WigEdit/${item.id}`}> */}
+                                                            <Button className="bg-[#303030] text-white hover:bg-red-500">Delete</Button>
+                                                            {/* </Link> */}
+                                                        </ButtonGroup>
 
-                                                </TableCell>
-                                            </TableRow>
+                                                    </TableCell>
+                                                </TableRow>
+                                            )
                                         ))}
                                     </TableBody>
                                 </Table>
                             </TableContainer>
-                            <TablePagination
-                                rowsPerPageOptions={[5, 10, 25, 50]}
-                                component="div"
-                                count={Member_Data.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                onPageChange={handleChangePage}
-                                onRowsPerPageChange={handleChangeRowsPerPage}
-                            />
+                            {loading ? (<Skeleton animation="wave" variant="text" className="w-1/5 text-2xl mb-2 rounded-md" />) : (
+                                <TablePagination
+                                    rowsPerPageOptions={[5, 10, 25, 50]}
+                                    component="div"
+                                    count={Member_Data.length}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    onPageChange={handleChangePage}
+                                    onRowsPerPageChange={handleChangeRowsPerPage}
+                                />
+                            )}
                         </Hidden>
                         <Grid item xs={12}>
                             <Hidden mdUp>
                                 {Member_Data.map((item, i) => (
-                                    <Accordion key={item.id} className="shadow-md">
-                                        <AccordionSummary>
-                                            <Typography className="font-semibold">{item.username}</Typography>
-                                        </AccordionSummary>
-                                        <AccordionDetails className="w-52 h-auto">
-                                            <img src={item.image} alt={item.username} />
-                                        </AccordionDetails>
-                                        <AccordionDetails className="bg-gray-50">
-                                            <Typography>Firstname: {item.firstname}</Typography><br />
-                                            <Typography>Lastname: {item.lastname}</Typography><br />
-                                            <Typography>Email: {item.email}</Typography><br />
-                                            <Typography>Password: {item.password}</Typography>
-                                        </AccordionDetails>
-                                        <AccordionActions>
-                                            <ButtonGroup variant="contained" className="gap-1" aria-label="contained button group">
-                                                <Link href="/admin/MemberEdit/[id]" as={`/admin/MemberEdit/${item.id}`}>
-                                                    <Button className="bg-[#303030] text-white hover:bg-amber-500">Edit</Button>
-                                                </Link>
-                                                <Button className="bg-[#303030] text-white hover:bg-red-500">Delete</Button>
-                                            </ButtonGroup>
-                                        </AccordionActions>
-                                    </Accordion>
+                                    loading ? (<Skeleton key={item.id} animation="wave" variant="rectangular" className="w-full h-10 my-2 rounded-md" />) : (
+                                        <Accordion key={item.id} className="shadow-md">
+                                            <AccordionSummary>
+                                                <Typography className="font-semibold">{item.username}</Typography>
+                                            </AccordionSummary>
+                                            <AccordionDetails className="w-52 h-auto">
+                                                <img src={item.image} alt={item.username} />
+                                            </AccordionDetails>
+                                            <AccordionDetails className="bg-gray-50">
+                                                <Typography>Firstname: {item.firstname}</Typography><br />
+                                                <Typography>Lastname: {item.lastname}</Typography><br />
+                                                <Typography>Email: {item.email}</Typography><br />
+                                                <Typography>Password: {item.password}</Typography>
+                                            </AccordionDetails>
+                                            <AccordionActions>
+                                                <ButtonGroup variant="contained" className="gap-1" aria-label="contained button group">
+                                                    <Link href="/admin/MemberEdit/[id]" as={`/admin/MemberEdit/${item.id}`}>
+                                                        <Button className="bg-[#303030] text-white hover:bg-amber-500">Edit</Button>
+                                                    </Link>
+                                                    <Button className="bg-[#303030] text-white hover:bg-red-500">Delete</Button>
+                                                </ButtonGroup>
+                                            </AccordionActions>
+                                        </Accordion>
+                                    )
                                 ))}
                             </Hidden>
                         </Grid>
