7aea970d386c1bd0a8bb1a0e3f55f1fca7b7b929
Heart Wirunrom <heart569522@gmail.com>
2023-02-02 02:01:55 +0700

    fix

---
+++ front/pages/api/member/addMember.js
@@ -0,0 +1,23 @@
+import clientPromise from "../../../lib/mongodb";
+
+export default async (req, res) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("high_u");
+    const { image, firstname, lastname, email, username, password } = req.body;
+
+    const member = await db.collection("member").insertOne({
+      image,
+      firstname,
+      lastname,
+      email,
+      username,
+      password,
+    });
+
+    res.json(member);
+  } catch (e) {
+    console.error(e);
+    throw new Error(e).message;
+  }
+};
