e20cf250daef2251f8bf45a7ff1234d50daec8a3
Heart Wirunrom <heart569522@gmail.com>
2023-02-08 00:20:59 +0700

    add wig

--- front/components/Form/AddWig_Form.tsx
+++ front/components/Form/AddWig_Form.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { storage } from '../../pages/api/firebaseConfig';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 import {
@@ -17,10 +17,13 @@ import {
   FormHelperText,
   Avatar,
   Link,
-  Skeleton
+  Skeleton,
+  IconButton
 } from "@mui/material";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import RotateLeftIcon from '@mui/icons-material/RotateLeft';
 
 import DrawerBar from '../Navigation/DrawerBar';
 
@@ -67,27 +70,130 @@ const AddWig_Form = () => {
 
   const [progress, setProgress] = useState(0)
   const [url, setUrl] = useState<string | null>(null)
+
   const [mainImage, setMainImage] = useState<File | null>(null);
-  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
-  const defaultImageUrl = 'https://i.pinimg.com/736x/85/6c/0c/856c0c237eec555ec901c7fd4a275ae3.jpg';
+  const [subImage1, setSubImage1] = useState<File | null>(null);
+  const [subImage2, setSubImage2] = useState<File | null>(null);
+  const [subImage3, setSubImage3] = useState<File | null>(null);
+  const [arImage, setArImage] = useState<File | null>(null);
+
+  const [messageMainImage, setMessageMainImage] = useState('');
+  const [previewMainImage, setPreviewMainImage] = useState<string | null>(null);
+
+  const [messageSubImage1, setMessageSubImage1] = useState('');
+  const [previewSubImage1, setPreviewSubImage1] = useState<string | null>(null);
 
   useEffect(() => {
-    if (!previewUrl) {
+    if (!previewMainImage) {
       return;
     }
-    return () => URL.revokeObjectURL(previewUrl);
-  }, [previewUrl]);
+    return () => URL.revokeObjectURL(previewMainImage);
+  }, [previewMainImage]);
+
+  useEffect(() => {
+    if (!previewSubImage1) {
+      return;
+    }
+    return () => URL.revokeObjectURL(previewSubImage1);
+  }, [previewSubImage1]);
 
   const handleMainImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const mainImage = e.target.files?.[0];
     if (!mainImage) {
       setMainImage(null);
-      setPreviewUrl(null);
+      setPreviewMainImage(null);
       return;
     }
-    setMainImage(e.target.files ? e.target.files[0] : null);
-    setPreviewUrl(URL.createObjectURL(mainImage));
-  }
+
+    const isValidType = mainImage.type === "image/jpeg" || mainImage.type === "image/png";
+    if (!isValidType) {
+      // Show an error message for invalid file type
+      setMessageMainImage("Invalid Image File Type")
+      setMainImage(null);
+      setPreviewMainImage(null);
+      return;
+    }
+
+    const image = new Image();
+    image.src = URL.createObjectURL(mainImage);
+    image.onload = () => {
+      if (image.width > 1080 || image.height > 1920) {
+        // Show an error message for image size too small
+        setMessageMainImage("File Size less than 1080x1920px")
+        setMainImage(null);
+        setPreviewMainImage(null);
+        return;
+      }
+
+      URL.revokeObjectURL(image.src);
+      setMainImage(mainImage);
+      setPreviewMainImage(URL.createObjectURL(mainImage));
+    };
+  };
+
+  const handleSubImage1Change = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const subImage1 = e.target.files?.[0];
+    if (!subImage1) {
+      setSubImage1(null);
+      setPreviewSubImage1(null);
+      return;
+    }
+
+    const isValidType = subImage1.type === "image/jpeg" || subImage1.type === "image/png";
+    if (!isValidType) {
+      // Show an error message for invalid file type
+      setMessageSubImage1("Invalid Image File Type")
+      setSubImage1(null);
+      setPreviewSubImage1(null);
+      return;
+    }
+
+    const image = new Image();
+    image.src = URL.createObjectURL(subImage1);
+    image.onload = () => {
+      if (image.width > 1080 || image.height > 1920) {
+        // Show an error message for image size too small
+        setMessageSubImage1("File Size less than 1080x1920px")
+        setSubImage1(null);
+        setPreviewSubImage1(null);
+        return;
+      }
+
+      URL.revokeObjectURL(image.src);
+      setSubImage1(subImage1);
+      setPreviewSubImage1(URL.createObjectURL(subImage1));
+    };
+  };
+
+  const handleMainImageReset = () => {
+    // setMainImage(null);
+    setPreviewMainImage(null);
+    setMessageMainImage("");
+  };
+
+  const handleSubImage1Reset = () => {
+    // setSubImage1(null);
+    setPreviewSubImage1(null);
+    setMessageSubImage1("");
+  };
+
+  const mainImageInputRef = useRef<HTMLInputElement>(null);
+  const subImage1InputRef = useRef<HTMLInputElement>(null);
+  const subImage2InputRef = useRef<HTMLInputElement>(null);
+  const subImage3InputRef = useRef<HTMLInputElement>(null);
+  const arImageInputRef = useRef<HTMLInputElement>(null);
+
+  const openMainImageDialog = () => {
+    if (mainImageInputRef.current) {
+      mainImageInputRef.current.click();
+    }
+  };
+
+  const openSubImage1Dialog = () => {
+    if (subImage1InputRef.current) {
+      subImage1InputRef.current.click();
+    }
+  };
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === " ") {
@@ -97,7 +203,7 @@ const AddWig_Form = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    
+
   }
 
   const handleReset = () => {
@@ -107,9 +213,10 @@ const AddWig_Form = () => {
     setPrice("");
     setSize("");
     setMainImage(null);
-    setPreviewUrl(null);
+    setPreviewMainImage(null);
     setError("");
   };
+
 
   return (
     <ThemeProvider theme={theme}>
@@ -131,150 +238,80 @@ const AddWig_Form = () => {
             </Grid>
           </Grid>
           <form onSubmit={handleSubmit} onReset={handleReset} className="pt-3">
-            <Grid container className="pt-3" spacing={3}>
-              <Grid item xs={12} md={4}>
-                {loading ? (<Skeleton animation="wave" variant="rectangular" className="w-full h-72 rounded-md" />) : (
-                  <center>
-                    <input
-                      accept="image/*"
-                      style={{ display: "none", }}
-                      id="upload-button"
-                      type="file"
-                      onChange={handleMainImageChange}
-                    />
-                    <img
-                      src={previewUrl || defaultImageUrl}
-                      className="rounded-lg object-top object-cover h-auto w-96"
-                    />
-                    <label htmlFor="upload-button">
-                      <Button
-                        variant='contained'
-                        className="bg-[#F0CA83] text-[#303030] font-bold mb-2 hover:bg-[#f3b94d] mt-3"
-                        component="span"
-                        startIcon={<AddAPhotoIcon />}
-                      >
-                        Add Image
-                      </Button>
-                    </label>
-                  </center>
-                )}
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={3}>
+                <Box className="px-6 pt-4 pb-8 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer">
+                  <input
+                    ref={mainImageInputRef}
+                    accept="image/*"
+                    style={{ display: "none", }}
+                    id="upload-button"
+                    type="file"
+                    // multiple
+                    onChange={handleMainImageChange}
+                  />
+                  <Typography className="text-gray-700 font-bold text-center" variant="h5">Main Image</Typography>
+                  {previewMainImage ? (
+                    <center>
+                      <img
+                        src={previewMainImage || ''}
+                        className="rounded-lg object-top object-cover h-auto w-80 py-2"
+                      />
+                      <Box className="text-center items-center justify-center flex flex-col">
+                        <IconButton onClick={handleMainImageReset} className='text-gray-400 hover:text-red-400'>
+                          <RotateLeftIcon className="w-8 h-8" fontSize='large' />
+                        </IconButton>
+                      </Box>
+                    </center>
+                  ) : (
+                    <>
+                      <Box className="text-center items-center justify-center flex flex-col hover:opacity-70" onClick={openMainImageDialog}>
+                        {messageMainImage ? <Typography className="text-red-500 font-bold py-2" variant="subtitle1">{messageMainImage}</Typography> : null}
+                        <CloudUploadIcon className="text-gray-400 w-20 h-20" fontSize='large' />
+                        <Typography className="text-gray-500 font-bold" variant="subtitle1">Click to upload</Typography>
+                        <Typography className="text-amber-500" variant="subtitle2">*Only Vertical Image</Typography>
+                        <Typography className="text-amber-500" variant="subtitle2">**PNG or JPG (Maximum 1080x1920px)</Typography>
+                      </Box>
+                    </>
+                  )}
+                </Box>
               </Grid>
-              <Grid item xs={12} md={8}>
-                {loading ? (<Skeleton animation="wave" variant="rectangular" className="w-full h-16 my-3 rounded-md" />) : (
-                  <Grid item xs={12}>
-                    <Typography className="text-[#303030] font-bold pb-2 text-lg">Title</Typography>
-                    <TextField
-                      type='text'
-                      value={title}
-                      fullWidth
-                      name='title'
-                      variant='outlined'
-                      className="font-bold rounded pb-3"
-                      onChange={(e) => setTitle(e.target.value)}
-                      onKeyDown={handleKeyPress}
-                      inputProps={{ style: { color: "#303030" } }}
-                      sx={{ color: '#303030' }}
-                      required
-                      focused
-                    />
-                  </Grid>
-                )}
-                {loading ? (<Skeleton animation="wave" variant="rectangular" className="w-full h-16 my-3 rounded-md" />) : (
-                  <Grid item xs={12}>
-                    <Typography className="text-[#303030] font-bold pb-2 text-lg">Color</Typography>
-                    <TextField
-                      type='text'
-                      value={color}
-                      fullWidth
-                      name='color'
-                      variant='outlined'
-                      className="font-bold rounded pb-3"
-                      onChange={(e) => setColor(e.target.value)}
-                      onKeyDown={handleKeyPress}
-                      inputProps={{ style: { color: "#303030" } }}
-                      sx={{ color: '#303030' }}
-                      required
-                      focused
-                    />
-                  </Grid>
-                )}
-                {loading ? (<Skeleton animation="wave" variant="rectangular" className="w-full h-16 my-3 rounded-md" />) : (
-                  <Grid item xs={12}>
-                    <Typography className="text-[#303030] font-bold pb-2 text-lg">Size</Typography>
-                    <TextField
-                      type='text'
-                      value={size}
-                      fullWidth
-                      name='size'
-                      variant='outlined'
-                      className="font-bold rounded pb-3"
-                      onChange={(e) => setSize(e.target.value)}
-                      onKeyDown={handleKeyPress}
-                      inputProps={{ style: { color: "#303030" } }}
-                      sx={{ color: '#303030' }}
-                      required
-                      focused
-                    />
-                  </Grid>
-                )}
-                {loading ? (<Skeleton animation="wave" variant="rectangular" className="w-full h-16 my-3 rounded-md" />) : (
-                  <Grid item xs={12}>
-                    <Typography className="text-[#303030] font-bold pb-2 text-lg">Price</Typography>
-                    <TextField
-                      type='text'
-                      value={price}
-                      fullWidth
-                      name='price'
-                      variant='outlined'
-                      className="font-bold rounded pb-3"
-                      onChange={(e) => setPrice(e.target.value)}
-                      onKeyDown={handleKeyPress}
-                      inputProps={{ style: { color: "#303030" } }}
-                      sx={{ color: '#303030' }}
-                      required
-                      focused
-                    />
-                  </Grid>
-                )}
-                {loading ? (<Skeleton animation="wave" variant="rectangular" className="w-full h-16 my-3 rounded-md" />) : (
-                  <Grid item xs={12}>
-                    <Typography className="text-[#303030] font-bold pb-2 text-lg">Description</Typography>
-                    <TextField
-                      type='text'
-                      value={desc}
-                      fullWidth
-                      name='desc'
-                      variant='outlined'
-                      className="font-bold rounded pb-3"
-                      onChange={(e) => setDesc(e.target.value)}
-                      onKeyDown={handleKeyPress}
-                      inputProps={{ style: { color: "#303030" } }}
-                      sx={{ color: '#303030' }}
-                      multiline
-                      maxRows={5}
-                      required
-                      focused
-                    />
-                  </Grid>
-                )}
-                <Grid item xs={12}>
-                  <Hidden mdDown>
-                    {loading ? (<Skeleton animation="wave" variant="rectangular" sx={{ float: 'right' }} className="w-1/5 justify-end h-10 my-1 rounded-md" />) : (
-                      <ButtonGroup variant="contained" className="gap-1" sx={{ float: 'right' }} aria-label="contained button group">
-                        <Button type='submit' className="bg-[#303030] text-white hover:bg-emerald-600">OK</Button>
-                        <Button type='reset' className="bg-[#303030] text-white hover:bg-red-500">Reset</Button>
-                      </ButtonGroup>
-                    )}
-                  </Hidden>
-                  <Hidden mdUp>
-                    {loading ? (<Skeleton animation="wave" variant="rectangular" className="w-full h-10 my-1 rounded-md" />) : (
-                      <ButtonGroup variant="contained" className="gap-1 my-3" fullWidth aria-label="contained button group">
-                        <Button type='submit' className="bg-[#303030] text-white hover:bg-emerald-600">OK</Button>
-                        <Button type='reset' className="bg-[#303030] text-white hover:bg-red-500">Reset</Button>
-                      </ButtonGroup>
-                    )}
-                  </Hidden>
-                </Grid>
+              <Grid item xs={12} md={3}>
+                <Box className="px-6 pt-4 pb-8 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer">
+                  <input
+                    ref={subImage1InputRef}
+                    accept="image/*"
+                    style={{ display: "none", }}
+                    id="upload-button"
+                    type="file"
+                    // multiple
+                    onChange={handleSubImage1Change}
+                  />
+                  <Typography className="text-gray-700 font-bold text-center" variant="h5">Sub Image 1</Typography>
+                  {previewSubImage1 ? (
+                    <center>
+                      <img
+                        src={previewSubImage1 || ''}
+                        className="rounded-lg object-top object-cover h-auto w-80 py-2"
+                      />
+                      <Box className="text-center items-center justify-center flex flex-col">
+                        <IconButton onClick={handleSubImage1Reset} className='text-gray-400 hover:text-red-400'>
+                          <RotateLeftIcon className="w-8 h-8" fontSize='large' />
+                        </IconButton>
+                      </Box>
+                    </center>
+                  ) : (
+                    <>
+                      <Box className="text-center items-center justify-center flex flex-col hover:opacity-70" onClick={openSubImage1Dialog}>
+                        {messageSubImage1 ? <Typography className="text-red-500 font-bold py-2" variant="subtitle1">{messageSubImage1}</Typography> : null}
+                        <CloudUploadIcon className="text-gray-400 w-20 h-20" fontSize='large' />
+                        <Typography className="text-gray-500 font-bold" variant="subtitle1">Click to upload</Typography>
+                        <Typography className="text-amber-500" variant="subtitle2">*Only Vertical Image</Typography>
+                        <Typography className="text-amber-500" variant="subtitle2">**PNG or JPG (Maximum 1080x1920px)</Typography>
+                      </Box>
+                    </>
+                  )}
+                </Box>
               </Grid>
             </Grid>
           </form>
