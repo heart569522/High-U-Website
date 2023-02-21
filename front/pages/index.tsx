1d316f4ffb1d84eb9c8f7cdad9a4833343de4786
Heart Wirunrom <heart569522@gmail.com>
2023-02-22 01:35:46 +0700

    splash screen failed

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,20 +1,62 @@
+// "use client"
+
+// import * as React from 'react';
+// import { useState, useEffect } from 'react';
+// import { useRouter } from 'next/router'
+// import { path } from 'animejs'
+
+// // IMPORT COMPONENT
+// import SplashScreen from '../components/Other/SplashScreen';
+// import Navbar from "../components/Navigation/Navigation"
+// import HomeBackground from "../components/Home/HomeBackground"
+// import HomeContent from "../components/Home/HomeContent"
+// import Footer from "../components/Footer/Footer"
+
+// export default function index() {
+//   const router = useRouter()
+//   const isHome = router.pathname === "/"
+//   const [isLoading, setIsLoading] = useState(isHome)
+
+//   useEffect(() => {
+//     if (isLoading) return
+//   }, [isLoading])
+
+//   return (
+//     <>
+//       {isLoading && isHome ? (
+//         <SplashScreen finishLoading={() => setIsLoading(false)} />
+//       ) : (
+//         <>
+//           <Navbar />
+//           <HomeBackground />
+//           <HomeContent />
+//           <Footer />
+//         </>
+//       )}
+//     </>
+
+//   )
+// }
+
 import * as React from 'react';
 
 // IMPORT COMPONENT
+import SplashScreen from '../components/Other/SplashScreen';
 import Navbar from "../components/Navigation/Navigation"
+import HomeBackground from "../components/Home/HomeBackground"
+import HomeContent from "../components/Home/HomeContent"
 import Footer from "../components/Footer/Footer"
-
-// IMPORT PAGE
-import Home from "./user/Home"
 
 export default function index() {
 
+
   return (
-    <div>
-        <Navbar />
-        <Home />
-        <Footer />
-    </div>
+    <>
+      <Navbar />
+      <HomeBackground />
+      <HomeContent />
+      <Footer />
+    </>
 
   )
 }
