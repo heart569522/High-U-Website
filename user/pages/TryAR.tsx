de42118e6964c10f10e666e49969d7771bbb77e8
Heart Wirunrom <heart569522@gmail.com>
2023-04-02 23:52:58 +0700

    Try AR

--- user/pages/TryAR.tsx
+++ user/pages/TryAR.tsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
-    Grid,
-    Paper,
-    Container,
-    Typography,
-    IconButton,
-    Modal,
-    Button,
-    Box,
-    Card,
-    CardMedia,
-    CardContent,
-    CardActionArea,
-    ImageList,
-    ImageListItem,
-    Backdrop
+  Grid,
+  Paper,
+  Container,
+  Typography,
+  IconButton,
+  Modal,
+  Button,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActionArea,
+  ImageList,
+  ImageListItem,
+  Backdrop
 } from '@mui/material'
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import ClearIcon from '@mui/icons-material/Clear';
@@ -23,9 +23,6 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import DownloadIcon from '@mui/icons-material/Download';
 import ShareIcon from '@mui/icons-material/Share';
 import { createTheme, ThemeProvider, } from '@mui/material/styles';
-import Webcam from 'react-webcam';
-import * as faceapi from 'face-api.js';
-import { WithFaceLandmarks } from 'face-api.js';
 
 // IMPORT COMPONENT
 import Navbar from "../components/Navigation/Navigation"
@@ -34,162 +31,125 @@ import AR_Data from '../helper/AR_Data.json';
 import Head from 'next/head';
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#F0CA83",
-        },
-        secondary: {
-            main: "#F08383",
-        },
+  palette: {
+    primary: {
+      main: "#F0CA83",
     },
-    typography: {
-        fontFamily: [
-            'Prompt, sans-serif'
-        ].join(','),
+    secondary: {
+      main: "#F08383",
     },
+  },
+  typography: {
+    fontFamily: [
+      'Prompt, sans-serif'
+    ].join(','),
+  },
 
 });
 
-const videoConstraints = {
-    width: 720,
-    height: 1280,
-    facingMode: "user"
-};
-
 export default function TryAR() {
 
-    const [modalOpen, setModalOpen] = useState(false);
-    const [image, setImage] = useState<string | null>(null);
-    const webcamRef = React.useRef<Webcam>(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
-    const [modelsLoaded, setModelsLoaded] = useState(false);
+  useEffect(() => {
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
 
-    useEffect(() => {
-        Promise.all([
-            faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-            faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-        ]).then(() => setModelsLoaded(true));
-    }, []);
+  function handleMessage(event: { data: { type: string; image: React.SetStateAction<string | null>; }; }) {
+    if (event.data.type === "screenshot") {
+      setImage(event.data.image);
+      setModalOpen(true);
+    }
+  }
 
-    const capture = async () => {
-        const image = webcamRef.current?.getScreenshot();
-        setImage(image as string | null);
+  const handleCapture = () => {
+    const iframe = iframeRef.current;
+    if (iframe) {
+      iframe.contentWindow?.postMessage({ type: 'capture' }, '*');
+    }
+  };
 
-        if (modelsLoaded && image && webcamRef.current?.video) {
-            const img = await faceapi.fetchImage(image);
-            const detections = await faceapi.detectAllFaces(img, new faceapi.TinyFaceDetectorOptions())
-                .withFaceLandmarks() as WithFaceLandmarks<faceapi.WithFaceDetection<{}>>[];
-            if (detections.length > 0) {
-                const canvas = document.createElement('canvas');
-                canvas.width = webcamRef.current?.video.videoWidth ?? 0;
-                canvas.height = webcamRef.current?.video.videoHeight ?? 0;
-                const ctx = canvas.getContext('2d');
-                if (ctx) {
-                    ctx.drawImage(webcamRef.current?.video, 0, 0);
-                    const resizedDetections = faceapi.resizeResults(detections, {
-                        width: detections[0].detection.box.width,
-                        height: detections[0].detection.box.height
-                    }).withFaceLandmarks();
-                    const faceLandmarks = resizedDetections[0].landmarks;
-                    const imgElement = document.createElement('img');
-                    imgElement.src = '../public/hair_models.png';
-                    imgElement.onload = () => {
-                        faceapi.matchDimensions(canvas, resizedDetections[0].detection);
-                        const wigSize = Math.abs(faceLandmarks[14].x - faceLandmarks[0].x);
-                        const wigWidth = wigSize * (imgElement.width / imgElement.height);
-                        const wigHeight = wigSize;
-                        const wigX = faceLandmarks[0].x - (wigWidth / 2);
-                        const wigY = faceLandmarks[0].y - (wigHeight / 2);
-                        const wigPosition = new faceapi.Rect(wigX, wigY, wigWidth, wigHeight);
-                        ctx.drawImage(imgElement, wigPosition.x, wigPosition.y, wigPosition.width, wigPosition.height);
-                        setImage(canvas.toDataURL('image/jpeg'));
-                        setModalOpen(true);
-                    };
-                }
-            }
-        }
-    };
+  return (
+    <ThemeProvider theme={theme}>
+      <Head><title>Try AR | High U</title></Head>
+      <Navbar />
+      <Paper className="bg-[#252525] h-screen max-[899px]:h-full max-[628px]:h-screen">
+        <Container maxWidth="xl" >
+          <Grid container>
+            <Grid item xs={12} className="pt-4 ">
+              <Typography variant='h3' className='text-[#F0CA83] text-center max-sm:text-[36px] font-bold'>TRY AR</Typography>
+            </Grid>
+            <Grid item xs={12} className="pb-2 ">
+              <Typography variant='h5' className="text-[#F0CA83] text-center max-sm:text-base">Snap photos and share like never before</Typography>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2} className="pt-3">
+            <Grid item sm={12} md={6}>
+              <Box className="w-full h-full border-2 border-[#F0CA83] rounded">
+                <center>
+                  <iframe
+                    ref={iframeRef}
+                    src="/trywig2D/index.html"
+                    className="w-full h-screen"
+                  />
+                  <Grid item xs={12} className="pt-5 text-center">
+                    <IconButton
+                      onClick={handleCapture}
+                      className="text-gray-800 m-auto bg-[#F0CA83] transition hover:bg-[#ffc14d]"
+                      size="large"
+                    >
+                      <CameraAltIcon fontSize="large" />
+                    </IconButton>
+                  </Grid>
+                </center>
+                {/* Caprture Result */}
+              </Box>
+            </Grid>
+            <Grid item sm={12} md={6}>
+              {AR_Data.map((item, i) =>
+                <Grid key={i} item xs={3} sm={3} md={4} className="inline-flex">
+                  <Card variant="outlined" className="content">
+                    <CardActionArea>
+                      <div className="content-overlay" />
+                      <CardMedia
+                        className="content-image object-top h-56 w-56 max-xl:h-48 max-lg:h-40 max-md:h-48 max-sm:h-28"
+                        component="img"
+                        image={item.image}
+                      />
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              )}
+            </Grid>
+            <Modal
+              className="p-4 fixed top-0 left-0 w-screen h-screen bg-black/60 flex justify-center items-center"
+              open={modalOpen}
+              onClose={() => setModalOpen(false)}
+            >
+              <Box className="text-right">
+                <img src={image as string | undefined} alt="Screenshot" className="border-[12px] border-[#646464] rounded scale-x-[-1]" />
+                <IconButton className="mx-1 mt-2 bg-[#F0CA83] text-black font-bold duration-200 hover:bg-red-400 hover:text-white">
+                  <FavoriteIcon />
+                </IconButton>
+                <IconButton className="mx-1 mt-2 bg-[#F0CA83] text-black font-bold duration-200 hover:bg-green-700 hover:text-white">
+                  <DownloadIcon />
+                </IconButton>
+                <IconButton className="mx-1 mt-2 bg-[#F0CA83] text-black font-bold duration-200 hover:bg-blue-500 hover:text-white">
+                  <ShareIcon />
+                </IconButton>
+                <IconButton className="mx-1 mt-2 bg-[#F0CA83] text-black font-bold duration-200 hover:bg-zinc-700 hover:text-white" onClick={() => setModalOpen(false)}>
+                  <ClearIcon />
+                </IconButton>
+              </Box>
+            </Modal>
 
+          </Grid>
+        </Container>
+      </Paper>
+    </ThemeProvider>
 
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Head><title>Try AR | High U</title></Head>
-            <Navbar />
-            <Paper className="bg-[#252525] h-screen max-[899px]:h-full max-[628px]:h-screen">
-                <Container maxWidth="xl" >
-                    <Grid container>
-                        <Grid item xs={12} className="pt-4 ">
-                            <Typography variant='h3' className='text-[#F0CA83] text-center max-sm:text-[36px] font-bold'>TRY AR</Typography>
-                        </Grid>
-                        <Grid item xs={12} className="pb-2 ">
-                            <Typography variant='h5' className="text-[#F0CA83] text-center max-sm:text-base">Snap photos and share like never before</Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={2} className="pt-3">
-                        <Grid item sm={12} md={6}>
-                            <Box className="w-full h-full ">
-                                <center>
-                                    <Webcam
-                                        className="border-2 border-[#F0CA83] rounded"
-                                        width={680}
-                                        height={1280}
-                                        ref={webcamRef}
-                                        screenshotFormat="image/jpeg"
-                                        mirrored
-                                        videoConstraints={videoConstraints}
-                                    />
-                                    <Grid item xs={12} className="pt-5 text-center">
-                                        <IconButton onClick={capture} className="text-gray-800 m-auto bg-[#F0CA83] transition hover:bg-[#ffc14d]" size='large'>
-                                            <CameraAltIcon fontSize='large' />
-                                        </IconButton>
-                                    </Grid>
-                                </center>
-                                {/* Caprture Result */}
-                            </Box>
-                        </Grid>
-                        <Grid item sm={12} md={6}>
-                            {AR_Data.map((item, i) =>
-                                <Grid key={i} item xs={3} sm={3} md={4} className="inline-flex">
-                                    <Card variant="outlined" className="content">
-                                        <CardActionArea>
-                                            <div className="content-overlay" />
-                                            <CardMedia
-                                                className="content-image object-top h-56 w-56 max-xl:h-48 max-lg:h-40 max-md:h-48 max-sm:h-28"
-                                                component="img"
-                                                image={item.image}
-                                            />
-                                        </CardActionArea>
-                                    </Card>
-                                </Grid>
-                            )}
-                        </Grid>
-                        <Modal
-                            className="p-4 fixed top-0 left-0 w-screen h-screen bg-black/60 flex justify-center items-center"
-                            open={modalOpen}
-                            onClose={() => setModalOpen(false)}
-                        >
-                            <Box className="text-right">
-                                <img src={image as string | undefined} alt="Captured image" className="border-[12px] border-[#646464] rounded" />
-                                <IconButton className="mx-1 mt-2 bg-[#F0CA83] text-black font-bold duration-200 hover:bg-red-400 hover:text-white">
-                                    <FavoriteIcon />
-                                </IconButton>
-                                <IconButton className="mx-1 mt-2 bg-[#F0CA83] text-black font-bold duration-200 hover:bg-green-700 hover:text-white">
-                                    <DownloadIcon />
-                                </IconButton>
-                                <IconButton className="mx-1 mt-2 bg-[#F0CA83] text-black font-bold duration-200 hover:bg-blue-500 hover:text-white">
-                                    <ShareIcon />
-                                </IconButton>
-                                <IconButton className="mx-1 mt-2 bg-[#F0CA83] text-black font-bold duration-200 hover:bg-zinc-700 hover:text-white" onClick={() => setModalOpen(false)}>
-                                    <ClearIcon />
-                                </IconButton>
-                            </Box>
-                        </Modal>
-
-                    </Grid>
-                </Container>
-            </Paper>
-        </ThemeProvider>
-
-    )
+  )
 }
