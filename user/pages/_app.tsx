99997c2df1b30e7fc2a82a9a247883d2da90de29
Heart Wirunrom <heart569522@gmail.com>
2023-04-08 05:31:48 +0700

    Update _app.tsx

--- user/pages/_app.tsx
+++ user/pages/_app.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import AOS from 'aos';
 import { SessionProvider } from "next-auth/react"
 
-import '../styles/tailwind.css';
 import 'aos/dist/aos.css';
 import '../styles/globals.css'
 import '../styles/lodingStyle.css'
