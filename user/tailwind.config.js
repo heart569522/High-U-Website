7a0fea6941008ee3a0b64fad89dde9914e9dbfb2
Heart Wirunrom <heart569522@gmail.com>
2023-04-08 19:48:21 +0700

    Update tailwind.config.js

--- user/tailwind.config.js
+++ user/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
+  content: [
+    // using ./src/ dir
     "./src/**/*.{js,ts,jsx,tsx}",
+    // using ./ dir
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    // add more paths here
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
