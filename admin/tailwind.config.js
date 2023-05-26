6ae5cf8da16d474bfd0626cbd5b9200d0d967286
Heart Wirunrom <heart569522@gmail.com>
2023-05-26 13:36:11 +0700

    Update tailwind.config.js

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
+    // using ./src/ dir
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // using ./ dir
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    // add more paths here
     "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // tremor module
   ],
   theme: {
